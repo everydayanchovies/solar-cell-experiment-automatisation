d7331019ad370b7e88b1009bd9e42d5b0251c782
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2021-11-18 12:06:28 +0100

    docstring

--- main.py
+++ main.py
@@ -1,60 +1,8 @@
-import pyvisa
 import matplotlib.pyplot as plt
 from matplotlib import rc
+from pythondaq.controllers.arduino_device import ArduinoVISADevice
 
 rc('text', usetex=True)
-
-
-class ArduinoVISADevice:
-    def __init__(self, port):
-        """
-        Initializes a ArduinoVISADevice instance.
-        :type port: String
-        :param port: The port corresponding to the arduino device. For example: ASRL/dev/cu.usbmodem1301::INSTR
-        """
-        self.port = port
-        self.rm = pyvisa.ResourceManager("@py")
-        self.device = self.open_device()
-
-    def open_device(self) -> pyvisa.Resource:
-        """
-        Opens a device through the PyVISA resource manager.
-        :return: The opened device.
-        """
-        return self.rm.open_resource(
-            resource_name=self.port,
-            read_termination="\r\n",
-            write_termination="\n"
-        )
-
-    def set_output_voltage(self, channel, value):
-        """
-        Sets the output voltage of a certain channel.
-        :type channel: int
-        :param channel: The output channel of which the voltage should be set. For example: 0.
-        :type value: float
-        :param value: The voltage to set the output channel to. For example: 2.2.
-        """
-        self.device.query(f"OUT:CH{channel}:VOLT {value}")
-
-    def get_output_voltage(self, channel) -> float:
-        """
-        Get the output voltage of a certain channel.
-        :type channel: int
-        :param channel: The channel of which the output voltage will be read. For example, 1.
-        :return: The output voltage of the specified channel.
-        """
-        return float(self.device.query(f"OUT:CH{channel}:VOLT?"))
-
-    def get_input_voltage(self, channel) -> float:
-        """
-        Get the input voltage of a certain channel.
-        :type channel: int
-        :param channel: The channel of which the input voltage will be read. For example, 1.
-        :return: The input voltage of the specified channel.
-        """
-        return float(self.device.query(f"MEAS:CH{channel}:VOLT?"))
-
 
 PORT = "ASRL/dev/cu.usbmodem1301::INSTR"
 
@@ -71,7 +19,7 @@ for i in range(0, 1024):
 
     U_led = U_ch1 - U_ch2
 
-    R = 200
+    R = 220
     I_led = U_led / R
 
     U_I_pairs.append((U_led, I_led))
---
+++ pythondaq/controllers/arduino_device.py
@@ -0,0 +1,71 @@
+"""Enables communication with a device running a VISA compatible firmware.
+
+    Typical usage example:
+
+    PORT = "ASRL/dev/cu.usbmodem1301::INSTR"
+
+    device = ArduinoVISADevice(port=PORT)
+
+    # an led is connected to the output channel 0
+    CH_LED = 0
+
+    # turn the led on
+    device.set_output_voltage(CH_LED, 1023)
+
+    # turn the led off
+    device.set_output_voltage(CH_LED, 0)
+"""
+
+import pyvisa
+
+
+class ArduinoVISADevice:
+    def __init__(self, port):
+        """
+        Initializes a ArduinoVISADevice instance.
+        :type port: String
+        :param port: The port corresponding to the arduino device. For example: ASRL/dev/cu.usbmodem1301::INSTR
+        """
+        self.port = port
+        self.rm = pyvisa.ResourceManager("@py")
+        self.device = self.__open_device()
+
+    def __open_device(self) -> pyvisa.Resource:
+        """
+        Opens a device through the PyVISA resource manager.
+        :return: The opened device.
+        """
+        return self.rm.open_resource(
+            resource_name=self.port,
+            read_termination="\r\n",
+            write_termination="\n"
+        )
+
+    def set_output_voltage(self, channel, value):
+        """
+        Sets the output voltage of a certain channel.
+        :type channel: int
+        :param channel: The output channel of which the voltage should be set. For example: 0.
+        :type value: float
+        :param value: The voltage to set the output channel to. For example: 2.2.
+        """
+        self.device.query(f"OUT:CH{channel}:VOLT {value}")
+
+    def get_output_voltage(self, channel) -> float:
+        """
+        Get the output voltage of a certain channel.
+        :type channel: int
+        :param channel: The channel of which the output voltage will be read. For example, 1.
+        :return: The output voltage of the specified channel.
+        """
+        return float(self.device.query(f"OUT:CH{channel}:VOLT?"))
+
+    def get_input_voltage(self, channel) -> float:
+        """
+        Get the input voltage of a certain channel.
+        :type channel: int
+        :param channel: The channel of which the input voltage will be read. For example, 1.
+        :return: The input voltage of the specified channel.
+        """
+        return float(self.device.query(f"MEAS:CH{channel}:VOLT?"))
+
