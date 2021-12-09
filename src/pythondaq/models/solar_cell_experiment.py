a5e77d33e3aaebec54417e3738859083c56a1202
everydayanchovies <4356678+everydayanchovies@users.noreply.github.com>
2021-12-09 12:46:52 +0100

    eod

---
+++ src/pythondaq/models/solar_cell_experiment.py
@@ -0,0 +1,188 @@
+import csv
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from pythondaq.controllers.arduino_device import list_devices, device_info, ArduinoVISADevice
+
+CH_VOUT = 0
+CH_U1 = 1
+CH_U2 = 2
+
+
+def
+
+def save_data_to_csv(filepath: str, headers: list[str], data: list[tuple]):
+    """
+    Saves the provided data to a csv file on disk.
+    :param filepath: the filepath of the file to be created
+    :param headers: a list of csv headers (strings)
+    :param data: a list of data rows (list of tuples)
+    """
+    with open(filepath, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=headers)
+
+        writer.writeheader()
+
+        def make_row(data_row):
+            row = {}
+            for i in range(len(data_row)):
+                row[headers[i]] = data_row[i]
+            return row
+
+        [writer.writerow(make_row(data_row)) for data_row in data]
+
+
+def clean_float_arr(a):
+    return np.array([s if isinstance(s, (int, float)) else 0 for s in a])
+
+
+def plot_u_i(u, u_err, i, i_err):
+    """
+    Shows a matplotlib plot of the provided U, I data.
+    :param u: voltage data (array like)
+    :param u_err: error on voltage data (array like)
+    :param i: current data (array like)
+    :param i_err: error on current data (array like)
+    """
+    plt.scatter(u, i)
+    plt.errorbar(u, i, xerr=u_err, yerr=i_err, linestyle='')
+    plt.xlabel(r"$U_{solarcell}$ [V]")
+    plt.ylabel(r"$I_{solarcell}$ [A]")
+
+    plt.show()
+
+
+def plot_p_r(p, p_err, r, r_err):
+    """
+    Shows a matplotlib plot of the provided U, I data.
+    :param p: power (array like)
+    :param p_err: error on power (array like)
+    :param r: resistance (array like)
+    :param r_err: error on resistance (array like)
+    """
+    plt.scatter(r, p)
+    plt.errorbar(r, p, xerr=r_err, yerr=p_err, linestyle='')
+    plt.ylabel(r"$R_{solarcell}$ [$\Omega$]")
+    plt.xlabel(r"$P_{solarcell}$ [W]")
+
+    plt.show()
+
+
+def plot_u_r(u, u_err, i, i_err):
+    _u = u
+    _u_err = u_err
+    _r = np.array(_u) / np.array(i)
+    _r_err = np.array(_u_err) / np.array(i_err)
+
+    u = []
+    u_err = []
+    r = []
+    r_err = []
+    for i in range(len(_u)):
+        if np.isnan(_r[i]) or np.isinf(_r[i]):
+            continue
+        r.append(_r[i])
+        r_err.append(_r_err[i])
+        u.append(_u[i])
+        u_err.append(_u_err[i])
+
+    plt.scatter(u, r)
+    plt.errorbar(u, r, xerr=u_err, yerr=r_err, linestyle='')
+    plt.xlabel(r"$U_{solarcell}$ [V]")
+    plt.ylabel(r"$R_{solarcell}$ [$\Omega$]")
+
+    plt.show()
+
+
+def u_p_for_u_i(u, u_err, i, i_err):
+    p = u * i
+    p_err = p * np.sqrt(u_err ** 2 + i_err ** 2)
+    return u, u_err, p, p_err
+
+
+class SolarCellExperiment:
+    """SolarCellExperiment is a model representing an experiment with an LED and a resistor in series.
+
+    Attributes:
+        visa: an instance of the ArduinoVISADevice controller
+        """
+
+    def __init__(self, port: str):
+        self.visa = ArduinoVISADevice(port)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            self.visa.close_device()
+        except AttributeError:
+            # ignore error while closing simulated device
+            pass
+
+    def measure_u_i_r(self, output_voltage: float, repeat: int = 1) -> \
+            ((float, float), (float, float), (float, float), float):
+        """
+        Takes a (repeated) measurement of the voltage and current (with uncertainty) across and through the LED.
+        Also sets the output voltage of the LED prior to taking the measurement.
+        :param output_voltage: sets this voltage as the LED voltage prior to measurement
+        :param repeat: number of times to repeat the measurement (for calculating uncertainty)
+        :return: a tuple of tuples ((u, u_err), (i, i_err), (r, r_err)
+        """
+        if output_voltage > 0.0:
+            self.visa.set_output_voltage(CH_VOUT, output_voltage)
+
+        u_i_r_pairs = [res for res in self.__recursive_u_i_r_measurement(repeat)]
+
+        voltage, current, resistance = list(zip(*u_i_r_pairs))
+
+        def value_with_uncertainty(values):
+            return np.mean(values), np.std(values) / np.sqrt(len(values))
+
+        return value_with_uncertainty(voltage), value_with_uncertainty(current), value_with_uncertainty(resistance), output_voltage
+
+    def __recursive_u_i_r_measurement(self, repeat: int = 1):
+        """
+        Private function which recursively measures the current and voltage through and over the LED.
+        I implemented the logic like this (instead of a for loop) to challenge myself.
+        :param repeat: carry variable containing the number of times to still repeat the measurement
+        :return: yields a single measurement (u, i) continuously
+        """
+        if repeat <= 0:
+            return False
+
+        voltage = self.visa.get_input_voltage(CH_U1) * 3.0  # 1:3 voltage splitter
+        current = self.visa.get_input_voltage(CH_U2) / 4.7
+        if current:
+            resistance = voltage / current
+        else:
+            resistance = np.inf
+
+        yield voltage, current, resistance
+        yield from self.__recursive_u_i_r_measurement(repeat - 1)
+
+    def scan_u_i_r(self, start_voltage: float, end_voltage: float, step_size: float, repeat: int = 1):
+        """
+        Take a series of voltage and current measurements of the LED while varying the output voltage.
+        :param start_voltage: beginning output voltage
+        :param end_voltage: final output voltage
+        :param step_size: step size of the varying voltage
+        :param repeat: number of times to repeat each single measurement
+        :return: yields a single measurement ((u, u_err), (i, i_err), (r, r_err)) continuously
+        """
+        if end_voltage < start_voltage:
+            raise ValueError(f"The start voltage ({start_voltage:.2f}) cannot be larger than the end voltage "
+                             f"({end_voltage:.2f}). Try swapping the start and end voltage.")
+
+        for u in np.arange(start_voltage, end_voltage + step_size, step_size):
+            yield self.measure_u_i_r(u, repeat)
+
+        self.visa.set_output_voltage(CH_VOUT, 0)
+
+        return True
+
+    def find_optimal_v_out(self):
+        pass
+
+
---
+++ src/pythondaq/views/solar_cell_cli.py
@@ -0,0 +1,233 @@
+from typing import Union
+from rich import print
+from rich.progress import Progress
+
+import click
+
+from pythondaq.models.solar_cell_experiment import list_devices, device_info, SolarCellExperiment, u_p_for_u_i, \
+    plot_u_i, plot_p_r, save_data_to_csv, plot_u_r
+
+
+def port_for_search_query(search_q) -> Union[str, None]:
+    """
+    Tries to retrieve a port for a given search query.
+    :param search_q: a string by which to filter the available ports
+    :return: either a single matching port or nothing
+    """
+    matching_devices = list_devices(search_q)
+
+    if not matching_devices:
+        print("No devices found for your search query, try searching less specifically")
+        return None
+
+    if len(matching_devices) > 1:
+        print(f"Your query yielded {len(matching_devices)} devices. Please specify a single device.")
+        [print(d) for d in matching_devices]
+        return None
+
+    return matching_devices[0]
+
+
+@click.group()
+def cmd_group():
+    pass
+
+
+@cmd_group.command("list")
+@click.option(
+    "-s",
+    "--search",
+    default="",
+    help="Filter the list of ports to the specified query",
+    show_default=True,
+    type=click.STRING
+)
+def ls(search):
+    """
+    Command with which to list the available devices.
+    :param search: filters the available devices by this string
+    """
+    if not search:
+        print("The following devices are connected to your computer:")
+    else:
+        print(f"The following devices match '{search}'")
+
+    [print(d) for d in list_devices(search)]
+
+
+@cmd_group.command()
+@click.argument("search_arg", type=click.STRING, required=False)
+@click.option(
+    "-s",
+    "--search",
+    default="",
+    help="Try to pin down one device matching the specified search query",
+    show_default=True,
+    type=click.STRING
+)
+def info(search, search_arg):
+    """
+    Shows the info of a requested device.
+    :param search: filters the available devices by this string
+    :param search_arg: see search
+    """
+    q = search if search else search_arg
+    if not q:
+        return print("Please specify which device to communicate with. For example: 'diode info arduino'")
+
+    print(device_info(port_for_search_query(q)))
+
+
+@cmd_group.command()
+@click.option(
+    "-u",
+    "--voltage",
+    default=0.0,
+    help="Set the output voltage before taking the measurement",
+    show_default=True,
+    type=click.FloatRange(0, 3.3),
+    required=False
+)
+@click.option(
+    "-p",
+    "--port",
+    help="The port of the Arduino device (can be a partial string)",
+    type=click.STRING,
+    required=True
+)
+@click.option(
+    "-r",
+    "--repeat",
+    help="Amount of times to repeat the measurement",
+    type=click.INT,
+    required=False,
+    default=1,
+)
+def measure(port, voltage, repeat):
+    """
+    Measures the current running through the LED and sets the output voltage prior to that.
+    """
+    port = port_for_search_query(port)
+    if not port:
+        return
+
+    if voltage:
+        print(f"V_out has been set to {voltage} V.")
+
+    (u, u_err), (i, i_err) = SolarCellExperiment(port).measure_u_i(voltage, repeat)
+    _, _, p, p_err = u_p_for_u_i(u, u_err, i, i_err)
+
+    if repeat > 1:
+        print(f"The current running through the solar panel is {i:.6f}±{i_err:.6f} A.")
+        print(f"The power is {p:.6f}±{p_err:.6f} W.")
+    else:
+        print(f"The current running through the solar panel is {i:.6f} A.")
+        print(f"The power is {p:.6f} W.")
+
+
+@cmd_group.command()
+@click.option(
+    "-p",
+    "--port",
+    help="The port of the Arduino device (can be a partial string)",
+    type=click.STRING,
+    required=True
+)
+@click.option(
+    "-a",
+    "--start",
+    help="Set the starting voltage of the measurement",
+    type=click.FloatRange(0, 3.3),
+    required=False,
+    default=0.0,
+)
+@click.option(
+    "-b",
+    "--end",
+    help="Set the ending voltage of the measurement",
+    type=click.FloatRange(0, 3.3),
+    required=False,
+    default=3.3,
+)
+@click.option(
+    "-s",
+    "--step",
+    help="Set the step size of the measurement",
+    type=click.FloatRange(0, 3.3),
+    required=True,
+)
+@click.option(
+    "-o",
+    "--output",
+    help="The file in which to save the measurement",
+    required=False,
+    default=None,
+)
+@click.option(
+    "-r",
+    "--repeat",
+    help="Amount of times to repeat the measurement",
+    type=click.INT,
+    required=False,
+    default=1,
+)
+@click.option(
+    "-g",
+    "--graph",
+    help="Plot the measurement",
+    is_flag=True,
+)
+def scan(port, start, end, step, output, repeat, graph):
+    """
+    Takes a series of measurements while varying the output voltage.
+    """
+    port = port_for_search_query(port)
+    if not port:
+        return
+
+    if step < 3.3 / 1024:
+        return print("The step size is smaller than the device can measure.")
+
+    m = SolarCellExperiment(port)
+
+    rows = []
+    with Progress() as progress:
+        task = progress.add_task("Gathering measurements...", total=(end - start))
+
+        for ((u, u_err), (i, i_err), (r, r_err), v_out) in m.scan_u_i_r(start, end, step, repeat):
+            _, _, p, p_err = u_p_for_u_i(u, u_err, i, i_err)
+            rows.append((v_out, u, u_err, i, i_err, p, p_err, r, r_err))
+            if repeat > 1:
+                print(f"{v_out}\t{u:.3f}±{u_err:.3f} V\t{i:.6f}±{i_err:.6f} A\t{p:.6f}±{p_err:.7f} W\t{r:.3f}±{r_err:.3f} Ohm")
+            else:
+                print(f"{v_out}\t{u:.3f} V\t{i:.6f} A\t{p:.6f} W\t{r:.3f} Ohm")
+
+            progress.update(task, advance=step)
+
+    if output:
+        save_data_to_csv(output, ["VOUT", "U", "U_err", "I", "I_err", "P", "P_err", "R", "R_err"], rows)
+
+    if graph:
+        v_out, u, u_err, i, i_err, p, p_err, r, r_err = zip(*rows)
+        plot_u_i(u, u_err, i, i_err)
+        plot_p_r(p, p_err, r, r_err)
+        plot_u_r(u, u_err, i, i_err)
+
+
+@cmd_group.command()
+@click.option(
+    "-p",
+    "--port",
+    help="The port of the Arduino device (can be a partial string)",
+    type=click.STRING,
+    required=True
+)
+def find_optimal_resistance(port):
+    port = port_for_search_query(port)
+    if not port:
+        return
+
+
+
+if __name__ == "__main__":
+    cmd_group()
